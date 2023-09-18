ef8f1692f10c36064f36e7d96236d082716bbe8e
LacerdaKris <k_cris_poa@hotmail.com>
2023-09-18 17:47:11 -0300

    adicionando exemplo de router

---
+++ vuejs/demo_vuerouter/src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const parametro = route.params.nome;
+</script>
+
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>{{ parametro }}</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
--- vuejs/demo_watch_fetch/src/components/SimNao.vue
+++ vuejs/demo_watch_fetch/src/components/SimNao.vue
@@ -2,7 +2,8 @@
 import { ref, watch } from "vue";
 
 const pergunta = ref("");
-const resposta = ref("Perguntas devem terminar com interrogação");
+const resposta = ref("*Perguntas devem terminar com interrogação*");
+const imagem = ref("");
 
 watch(pergunta, async (novaPergunta) => {
   if (novaPergunta.indexOf("?") > -1) {
@@ -11,6 +12,7 @@ watch(pergunta, async (novaPergunta) => {
       const res = await fetch("https://yesno.wtf/api");
       const json = await res.json();
       resposta.value = json.answer;
+      imagem.value = json.image;
     } catch (error) {
       resposta.value = "Erro! API inacessível: " + error;
     }
@@ -24,4 +26,5 @@ watch(pergunta, async (novaPergunta) => {
     <input type="text" v-model="pergunta" />
   </p>
   <p>{{ resposta }}</p>
+  <img :src="imagem" alt="Resposta da imagem" />
 </template>
